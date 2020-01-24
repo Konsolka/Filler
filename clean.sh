29c628863682d23128e7eceb0c9587a2a7c22eea
Alexandr <Konsolka@users.noreply.github.com>
2020-01-24 13:53:41 +0300

    has been modified

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 files="$(find . -name "*.log")"
+files_trace="$(find . -name "filler.trace")"
 if [ "$files" = "" ]; then
-	echo "Nothing to delete"
-	exit 1
+	echo " - No *.log files were found or they do not exist"
 else
 	echo " - Deliting"
 	find . -name "*.log"
@@ -10,11 +10,25 @@ else
 	case "$ans" in
 		"y"|"Y"|"Yes"|"yes")
 			find . -name "*.log" -delete
-			echo " - Files deleted"
+			echo " - Files has been deleted"
 			;;
 		"n"|"N"|"no"|"No")
-			echo " - Exiting"
-			exit 1
 			;;
 	esac
 fi
+
+if [ "$files_trace" = "" ]; then
+	echo " - Exiting"
+	exit 1
+fi
+echo " - Delete filler trace? [y/n]"
+read ans
+case "$ans" in
+	"y"|"Y"|"Yes"|"yes")
+		find . -name "filler.trace" -delete
+		echo " - filler.trace has been deleted"
+		;;
+	"n"|"N"|"no"|"No")
+		echo " - Exiting"
+		;;
+esac
